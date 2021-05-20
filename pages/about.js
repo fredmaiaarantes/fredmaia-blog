80d5062c4dcda8aa3a6be51333702a0e71a42a39
Frederico Maia <fredericomaia10@gmail.com>
2021-05-20 22:19:07 +0100

    - Blog changes

--- pages/about.js
+++ pages/about.js
@@ -22,32 +22,51 @@ export default function About() {
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
               {siteMetadata.author}
             </h3>
-            <div className="text-gray-500 dark:text-gray-400">Professor of Atmospheric Science</div>
-            <div className="text-gray-500 dark:text-gray-400">Stanford University</div>
+            <div className="text-gray-500 dark:text-gray-400">
+              Tech Lead & Senior Software Developer
+            </div>
+            <div className="text-gray-500 dark:text-gray-400">Quave / Pathable</div>
             <div className="flex pt-6 space-x-3">
-              <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
-              <SocialIcon kind="github" href={siteMetadata.github} />
-              <SocialIcon kind="facebook" href={siteMetadata.facebook} />
-              <SocialIcon kind="youtube" href={siteMetadata.youtube} />
-              <SocialIcon kind="linkedin" href={siteMetadata.linkedin} />
               <SocialIcon kind="twitter" href={siteMetadata.twitter} />
+              <SocialIcon kind="linkedin" href={siteMetadata.linkedin} />
+              <SocialIcon kind="github" href={siteMetadata.github} />
+              <SocialIcon kind="youtube" href={siteMetadata.youtube} />
             </div>
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
             <p>
-              Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His
-              research interests includes complexity modelling of tailwinds, headwinds and
-              crosswinds.
+              I've been working with software development since 2008, today working at Quave (a
+              Brazilian company), working at the services division leading a team and developing
+              software for Pathable (an American company). I'm Brazilian, lived in Ireland for more
+              than 3 years, and now in Portugal but living my dream of being a digital nomad.
+            </p>
+
+            <p>
+              I worked in amazing Brazilian companies such as Caelum, TecSinapse, and Petrobras, as
+              well as multinationals such as Equifax and LeasePlan in Ireland. I have extensive
+              experience in Java, JavaScript, and TypeScript. Today, in addition to continuing my
+              development studies, I'm also dedicated to learning about digital marketing and
+              entrepreneurship.
             </p>
             <p>
-              He leads the clean energy group which develops 3D air pollution-climate models, writes
-              differential equation solvers, and manufactures titanium plated air ballons. In his
-              free time he bakes raspberry pi.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique
-              placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem
-              nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
+              Created the group{' '}
+              <a href="https://linktr.ee/nerdsnaeuropa" target="blank">
+                Nerds na Europa
+              </a>
+              , creating content on my{' '}
+              <a href="https://www.youtube.com/user/fredericomaia10/" target="blank">
+                Youtube channel
+              </a>{' '}
+              about development and career abroad. With my wife, I also produce videos on{' '}"
+              <a href="https://www.youtube.com/GoianosnaIrlanda" target="blank">
+                Goianos na Irlanda
+              </a>
+              ", where they do interviews and share their own experiences about life abroad. He
+              trained more than 50 students on his course "
+              <a href="https://devnairlanda.com" target="blank">
+                Dev na Irlanda
+              </a>
+              ", created for developers looking for a career in Ireland.
             </p>
           </div>
         </div>
