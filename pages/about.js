010e3eedb444dd23d0969de9870c435124b0f790
Frederico Maia <fredericomaia10@gmail.com>
2021-05-21 13:27:14 +0100

    - Blog changes

--- pages/about.js
+++ pages/about.js
@@ -35,10 +35,14 @@ export default function About() {
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
             <p>
-              I've been working with software development since 2008, today working at Quave (a
-              Brazilian company), working at the services division leading a team and developing
-              software for Pathable (an American company). I'm Brazilian, lived in Ireland for more
-              than 3 years, and now in Portugal but living my dream of being a digital nomad.
+              Hey, I'm Fred. I'm software developer since 2008, today working at Quave (a Brazilian
+              company), working at the services division leading a team and developing software for
+              Pathable (an American company).
+            </p>
+            <p>
+              I'm Brazilian, I lived in Ireland for more than 3 years, and now I'm in Portugal but
+              living my dream of being a digital nomad, travelling and working from new places every
+              month.
             </p>
 
             <p>
@@ -49,25 +53,28 @@ export default function About() {
               entrepreneurship.
             </p>
             <p>
-              Created the group{' '}
+              I'm the creator of the group{' '}
               <a href="https://linktr.ee/nerdsnaeuropa" target="blank">
                 Nerds na Europa
-              </a>
-              , creating content on my{' '}
+              </a>{' '}
+              and I create content on my{' '}
               <a href="https://www.youtube.com/user/fredericomaia10/" target="blank">
                 Youtube channel
               </a>{' '}
-              about development and career abroad. With my wife, I also produce videos on{' '}"
+              about development and career abroad. With my wife, I also produce videos on{' '}
               <a href="https://www.youtube.com/GoianosnaIrlanda" target="blank">
                 Goianos na Irlanda
               </a>
-              ", where they do interviews and share their own experiences about life abroad. He
-              trained more than 50 students on his course "
-              <a href="https://devnairlanda.com" target="blank">
-                Dev na Irlanda
-              </a>
-              ", created for developers looking for a career in Ireland.
+              , where we do interviews and share our experience living abroad.
             </p>
+            <div className="pt-8 prose dark:prose-dark max-w-none xl:col-span-2">
+              <iframe
+                title="Fred Travel Map"
+                src="https://www.google.com/maps/d/u/0/embed?mid=1Evn-GhfPxEptsutoiNJDhK8OJ4LP4gNQ"
+                width="100%"
+                height="320"
+              />
+            </div>
           </div>
         </div>
       </div>
